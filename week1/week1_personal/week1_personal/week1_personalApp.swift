b50328267262ed5261d0a7891a44abe4e08ffb03
2021056317 <myday2day@hanyang.ac.kr>
2023-09-27 15:10:46 +0900

    Create umc week1 personal
    umc week1 personal

---
+++ week1/week1_personal/week1_personal/week1_personalApp.swift
@@ -0,0 +1,17 @@
+//
+//  week1_personalApp.swift
+//  week1_personal
+//
+//  Created by 유철민 on 2023/09/27.
+//
+
+import SwiftUI
+
+@main
+struct week1_personalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
