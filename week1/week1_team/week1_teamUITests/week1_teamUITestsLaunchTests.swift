6f5fb4021d0b519833fee46cb9c40b54f251c701
2021056317 <myday2day@hanyang.ac.kr>
2023-10-05 21:56:37 +0900

    week2
    week2

---
+++ week1/week1_team/week1_teamUITests/week1_teamUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  week1_teamUITestsLaunchTests.swift
+//  week1_teamUITests
+//
+//  Created by 유철민 on 2023/09/27.
+//
+
+import XCTest
+
+final class week1_teamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ week2/week_2_p/week_2_p/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week_2_p
+//
+//  Created by 유철민 on 2023/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
