34c354c8df8c989e599d348401bf1d08e1392a5e
2021056317 <myday2day@hanyang.ac.kr>
2023-10-06 15:42:08 +0900

    week2_team

---
+++ week2/week2_team/week2_team/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week2_team
+//
+//  Created by 유철민 on 2023/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
