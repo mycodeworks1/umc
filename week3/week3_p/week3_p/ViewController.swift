59b86f2bee60696ad8550807f652ecc04fd6d349
2021056317 <myday2day@hanyang.ac.kr>
2023-10-11 17:46:18 +0900

    week3_p

---
+++ week3/week3_p/week3_p/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week3_p
+//
+//  Created by 유철민 on 2023/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ week3/week3_p/week3_p/cell/CarrotTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CarrotTableViewCell.swift
+//  week3_p
+//
+//  Created by 유철민 on 2023/10/11.
+//
+
+import UIKit
+
+class CarrotTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
