66ba7d2a8fbbcfb5a66537d8d81a43b03386bc65
2021056317 <myday2day@hanyang.ac.kr>
2023-11-23 14:17:07 +0900

    week6

---
+++ week6/weak6_p/weak6_p/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weak6_p
+//
+//  Created by 유철민 on 2023/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ week6/weak6_p/weak6_pUITests/weak6_pUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  weak6_pUITestsLaunchTests.swift
+//  weak6_pUITests
+//
+//  Created by 유철민 on 2023/11/15.
+//
+
+import XCTest
+
+final class weak6_pUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
