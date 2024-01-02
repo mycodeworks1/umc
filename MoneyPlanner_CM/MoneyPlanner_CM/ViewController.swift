2682ba04d59173eb9412a4cf3413d72eb2b061c2
2021056317 <myday2day@hanyang.ac.kr>
2024-01-03 02:42:47 +0900

    [add] init
    * 개인적으로 머플러를 위해 연습

---
+++ MoneyPlanner_CM/MoneyPlanner_CM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyPlanner_CM
+//
+//  Created by 유철민 on 1/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+    }
+
+}
+
